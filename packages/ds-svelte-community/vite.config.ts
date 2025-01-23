a872436452f8d67595991fef62e8a7fe45fb096d
Thomas Krampl <85170275+thokra-nav@users.noreply.github.com>
2025-01-23 15:47:48 +0100

    upgrade dependencies (#203)
    * upgrade dependencies
    
    * Fix changes with tailwind

--- packages/ds-svelte-community/vite.config.ts
+++ packages/ds-svelte-community/vite.config.ts
@@ -1,11 +1,13 @@
 import docProcess from "@nais/vite-plugin-svelte-docs";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 import storyProcess from "./src/doclib/preprocessor";
 
 export default defineConfig({
 	plugins: [
+		tailwindcss() as never, // Unsure why this is necessary
 		storyProcess(),
 		sveltekit() as never, // Unsure why this is necessary
 		docProcess(path.resolve("..", "..", "node_modules", "svelte2tsx", "index.js")) as never,
