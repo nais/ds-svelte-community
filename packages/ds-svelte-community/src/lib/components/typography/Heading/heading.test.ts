ce2ea89b6af79eaee246cce808ee0cb57cad146c
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-10-09 10:09:42 +0200

    Support constrained polymorphic elements
    Use with Heading

--- packages/ds-svelte-community/src/lib/components/typography/Heading/heading.test.ts
+++ packages/ds-svelte-community/src/lib/components/typography/Heading/heading.test.ts
@@ -7,26 +7,23 @@ import Heading from "./Heading.svelte";
 import type { HeadingProps } from "./type";
 
 describe("Heading", () => {
-	[undefined, "h1", "h2"].forEach((as) => {
+	[undefined, "h1", "h2", "h3", "h4", "h5", "h6"].forEach((as) => {
 		["xlarge", "large", "medium", "small", "xsmall"].forEach((size) => {
 			[undefined, true].forEach((spacing) => {
-				new Array(6).fill(1).forEach((_, level) => {
-					it(`renders with HTML similar to ds-react (as: ${as} size: ${size} spacing: ${spacing} level: ${level})`, async () => {
-						const props: HeadingProps = {
-							as: as as never,
-							size: size as never,
-							spacing,
-							level: (level + 1) as never,
-							children: createRawSnippet(() => ({
-								render() {
-									return "<span>Heading content</span>";
-								},
-							})),
-						};
-						expect(render(Heading, props)).toMimicReact(ReactHeading, {
-							props,
-							children: [React.createElement("span", {}, "Heading content")],
-						});
+				it(`renders with HTML similar to ds-react (as: ${as} size: ${size} spacing: ${spacing})`, async () => {
+					const props: HeadingProps = {
+						as: as as never,
+						size: size as never,
+						spacing,
+						children: createRawSnippet(() => ({
+							render() {
+								return "<span>Heading content</span>";
+							},
+						})),
+					};
+					expect(render(Heading, props)).toMimicReact(ReactHeading, {
+						props,
+						children: [React.createElement("span", {}, "Heading content")],
 					});
 				});
 			});
