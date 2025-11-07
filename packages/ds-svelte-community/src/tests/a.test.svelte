7cee728c6b9c6110c35504c2f1969fea07cf5d13
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-11-07 15:04:24 +0100

    Fix anchor colors

---
+++ packages/ds-svelte-community/src/tests/a.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	interface Props {
+		href?: string;
+		inlineText?: boolean;
+	}
+
+	let { href = "#", inlineText = false }: Props = $props();
+</script>
+
+{#if inlineText}
+	<p>
+		Some text before
+		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+		<a {href}>Link text</a>
+		some text after.
+	</p>
+{:else}
+	<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+	<a {href}>Link text</a>
+{/if}
